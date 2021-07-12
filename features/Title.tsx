fdf144de817ab17df01d5dbca61e625782870c9b
JJ Yang <rongyang610@gmail.com>
2021-07-12 02:37:59 -0400

    move Text and Row to components

--- features/Title.tsx
+++ features/Title.tsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import Row from 'component/Row'
+import Text from 'component/Text'
+
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 16px;
   width: 100%;
 `
-const Row = styled.div`
-  display: flex;
-`
+
 const Header = styled.h1`
   font-weight: ${({ $weight }: HeaderProps) => ($weight ? $weight : 700)};
   margin: 0;
 `
-const Text = styled.span`
-  font-size: ${({ $size }: TextProps) => ($size ? $size : '16px')};
-  font-weight: ${({ $weight }) => ($weight ? $weight : 300)};
-`
+
 const TimerRow = styled(Row)`
   justify-content: flex-end;
   margin-bottom: 8px;
@@ -44,10 +42,6 @@ const Title = ({ time }: TitleProps) => {
 }
 
 type HeaderProps = {
-  $weight?: number
-}
-type TextProps = {
-  $size?: string
   $weight?: number
 }
 type TitleProps = {
