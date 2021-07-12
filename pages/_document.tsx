5a5dd1637b28e656f46cb1df25c8bf9d6a5b2137
JJ Yang <rongyang610@gmail.com>
2021-07-12 01:35:54 -0400

    feat(font-family): add lato font-family to head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,12 @@ class MyDocument extends Document {
             name="description"
             content="ColourLovers Live is an exploration of british spelling and intended to keep software engineers familiar with the latest trends in colour palettes."
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&display=swap"
+            rel="stylesheet"
+          />
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
