21ecf83712786ed80bf1f2aae32d82ba09381335
JJ Yang <rongyang610@gmail.com>
2021-07-12 01:21:58 -0400

    feat(document): create custom document and move favicon & description

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="description"
+            content="ColourLovers Live is an exploration of british spelling and intended to keep software engineers familiar with the latest trends in colour palettes."
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,6 @@ export default function Home() {
     <div>
       <Head>
         <title>ColourLover</title>
-        <meta
-          name="description"
-          content="ColourLovers Live is an exploration of british spelling and intended to keep software engineers familiar with the latest trends in colour palettes"
-        />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
     </div>
   );
