da561f49c630e9f5e7a0f1ca5c6d06eb970c54a4
JJ Yang <rongyang610@gmail.com>
2021-07-12 01:44:23 -0400

    feat(home-page): add initial styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import Head from 'next/head'
+import styled, { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #1b89d0;
+    font-family: Lato;
+    color: white;
+    font-size: 16px;
+  }
+`
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>ColourLover</title>
       </Head>
-    </div>
+      <GlobalStyle />
+      <Wrapper></Wrapper>
+    </>
   )
 }
